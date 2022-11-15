af9828f7de53edf4cff7d0a230628bc4566accbf
rickiestmrty <nestordesabille@gmail.com>
2022-11-15 07:33:28 +0000

    Copy paste code

--- raspi_stepper.py
+++ raspi_stepper.py
@@ -1,27 +1,20 @@
-from operator import truediv
 from time import sleep
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
-DIR = 20                # Direction GPIO Pin
-STEP = 21               # Step GPIO Pin
-ROTATION: bool = 1      # Direction of rotation (1 - CW, 0 - CCW)
-SPR = 48                # Steps per Revolution (360 / 7.5)
-CURR_STEP_SOLAR = 0     # Tracks the position of the x-axis stepper motor for solar
-CURR_STEP_WIND = 0      # Tracks the position of the x-axis stepper motor for wind
+DIR = 20   # Direction GPIO Pin
+STEP = 21  # Step GPIO Pin
+CW = 1     # Clockwise Rotation
+CCW = 0    # Counterclockwise Rotation
+SPR = 48   # Steps per Revolution (360 / 7.5)
 
-'''
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(DIR, GPIO.OUT)   # direction pin is out
-GPIO.setup(STEP, GPIO.OUT)  # step pin is out
-GPIO.output(DIR, ROTATION)        # clockwise direction
-'''
+GPIO.setup(DIR, GPIO.OUT)
+GPIO.setup(STEP, GPIO.OUT)
+GPIO.output(DIR, CW)
 
-step_cnt = SPR
-delay = 0.0208
+step_count = SPR
+delay = .0208
 
-''' 360 rotation and back
-
-# Clockwise
 for x in range(step_count):
     GPIO.output(STEP, GPIO.HIGH)
     sleep(delay)
@@ -29,48 +22,11 @@ for x in range(step_count):
     sleep(delay)
 
 sleep(.5)
-
-# Counter Clockwise
-ROTATION = 0
-GPIO.output(DIR, ROTATION)
+GPIO.output(DIR, CCW)
 for x in range(step_count):
     GPIO.output(STEP, GPIO.HIGH)
     sleep(delay)
     GPIO.output(STEP, GPIO.LOW)
     sleep(delay)
 
-'''
-
-#GPIO.cleanup()
-
-def solar_x(steps):
-
-    for x in range(steps):
-        if CURR_STEP_SOLAR == SPR:
-            ROTATION = not ROTATION
-            #GPIO.output(DIR,ROTATION)
-            CURR_STEP_SOLAR = 0
-        #GPIO.output(STEP, GPIO.HIGH)
-        sleep(delay)
-        #GPIO.output(STEP, GPIO.LOW)
-        sleep(delay)
-        wind(1,True)
-        CURR_STEP_SOLAR += 1
-
-def wind(steps,solar=False):
-
-    for x in range(steps):
-        if solar:
-            wind_rotation = not ROTATION
-        else:
-            wind_rotation = ROTATION
-        if CURR_STEP_WIND == SPR:
-            wind_rotation = not wind_rotation
-            #GPIO.output(DIR,ROTATION)
-            CURR_STEP_WIND = 0
-        #GPIO.output(STEP, GPIO.HIGH)
-        sleep(delay)
-        #GPIO.output(STEP, GPIO.LOW)
-        sleep(delay)
-        CURR_STEP_WIND += 1
-
+GPIO.cleanup()

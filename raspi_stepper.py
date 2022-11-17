cb4a9d407723362efbec47a5a09201a2b7df157a
nestor <nestordesabille4@gmail.com>
2022-11-17 16:36:55 +0800

    Change microstep to 32

--- raspi_stepper.py
+++ raspi_stepper.py
@@ -12,8 +12,18 @@ GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
 GPIO.output(DIR, CW)
 
-step_count = SPR
-delay = .0208
+MODE = (14, 15, 18)   # Microstep Resolution GPIO Pins
+GPIO.setup(MODE, GPIO.OUT)
+RESOLUTION = {'Full': (0, 0, 0),
+              'Half': (1, 0, 0),
+              '1/4': (0, 1, 0),
+              '1/8': (1, 1, 0),
+              '1/16': (0, 0, 1),
+              '1/32': (1, 0, 1)}
+GPIO.output(MODE, RESOLUTION['1/32'])
+
+step_count = SPR * 32
+delay = .0208 / 32
 
 for x in range(step_count):
     GPIO.output(STEP, GPIO.HIGH)

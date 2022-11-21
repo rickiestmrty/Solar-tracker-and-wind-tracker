97f067d132309703c3afb048635dc28ba8889456
rickiestmrty <nestordesabille@gmail.com>
2022-11-21 09:47:34 +0000

    correct SPR

--- raspi_stepper.py
+++ raspi_stepper.py
@@ -1,42 +1,46 @@
 from time import sleep
 import RPi.GPIO as GPIO
 
-DIR = 20   # Direction GPIO Pin
-STEP = 21  # Step GPIO Pin
-CW = 1     # Clockwise Rotation
-CCW = 0    # Counterclockwise Rotation
-SPR = 48   # Steps per Revolution (360 / 7.5)
+DIR = 20                # Direction GPIO Pin
+STEP = 21               # Step GPIO Pin
+SPR = 200                # Steps per Revolution (360 / 1.8)
+curr_step = 0           # Current position
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(DIR, GPIO.OUT)
-GPIO.setup(STEP, GPIO.OUT)
-GPIO.output(DIR, CW)
+def initialize(direction):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(DIR, GPIO.OUT)
+    GPIO.setup(STEP, GPIO.OUT)
+    GPIO.output(DIR,direction)
 
-MODE = (14, 15, 18)   # Microstep Resolution GPIO Pins
-GPIO.setup(MODE, GPIO.OUT)
-RESOLUTION = {'Full': (0, 0, 0),
-              'Half': (1, 0, 0),
-              '1/4': (0, 1, 0),
-              '1/8': (1, 1, 0),
-              '1/16': (0, 0, 1),
-              '1/32': (1, 0, 1)}
-GPIO.output(MODE, RESOLUTION['1/32'])
+    MODE = (14, 15, 18)   # Microstep Resolution GPIO Pins
+    GPIO.setup(MODE, GPIO.OUT)
+    RESOLUTION = {'Full': (0, 0, 0),
+                'Half': (1, 0, 0),
+                '1/4': (0, 1, 0),
+                '1/8': (1, 1, 0),
+                '1/16': (0, 0, 1),
+                '1/32': (1, 0, 1)}
+    GPIO.output(MODE, RESOLUTION['Full'])
 
-step_count = SPR * 32
-delay = .0208 / 32
+def rotate_motor(steps, direction):
+    global curr_step
+    initialize(direction)
+    delay = .0208
+    for x in range(steps):
+        GPIO.output(STEP, GPIO.HIGH)
+        sleep(delay)
+        GPIO.output(STEP, GPIO.LOW)
+        sleep(delay)
+        curr_step += 1
+        '''
+        if curr_step > 200:
+            curr_step = 0
+            rotate_motor(200, direction^1)
+            initialize(direction)
+            curr_step = 0'''
 
-for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
-    sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
-    sleep(delay)
+    GPIO.cleanup()
 
-sleep(.5)
-GPIO.output(DIR, CCW)
-for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
-    sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
-    sleep(delay)
-
-GPIO.cleanup()
+rotate_motor(200, 1)
+rotate_motor(200, 0)
+rotate_motor(200, 1)

8d3fc31450ad4af39c4c968e3442aa4257ce8897
nestor <nestordesabille4@gmail.com>
2022-12-03 13:16:53 +0800

    Convert to raspi_stepper.py to a class

--- raspi_stepper.py
+++ raspi_stepper.py
@@ -1,46 +1,46 @@
 from time import sleep
 import RPi.GPIO as GPIO
 
-DIR = 20                # Direction GPIO Pin
-STEP = 21               # Step GPIO Pin
-SPR = 200               # Steps per Revolution (360 / 1.8)
-curr_angle = 0          # Current position
 
-def initialize(direction):
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(DIR, GPIO.OUT)
-    GPIO.setup(STEP, GPIO.OUT)
-    GPIO.output(DIR,direction)
+class StepperMotor:
 
-    MODE = (14, 15, 18)   # Microstep Resolution GPIO Pins
-    GPIO.setup(MODE, GPIO.OUT)
-    RESOLUTION = {'Full': (0, 0, 0),
-                'Half': (1, 0, 0),
-                '1/4': (0, 1, 0),
-                '1/8': (1, 1, 0),
-                '1/16': (0, 0, 1),
-                '1/32': (1, 0, 1)}
-    GPIO.output(MODE, RESOLUTION['Full'])
+    def __init__(self,offset) -> None:
+        self.OFFSET = offset
+        self.DIR = 20                # Direction GPIO Pin
+        self.STEP = 21               # Step GPIO Pin
+        self.SPR = 200               # Steps per Revolution (360 / 1.8)
+        self.curr_angle = 0          # Current position
 
-def rotate_motor(angle):
-    global curr_angle
-    angle_diff = angle - curr_angle
-    steps = int(abs(angle_diff) * 0.55556)
-    direction = 0 if angle_diff < 0 else 1
-    initialize(direction)
-    print(steps)
-    delay = .0208
-    for x in range(steps):
-        GPIO.output(STEP, GPIO.HIGH)
-        sleep(delay)
-        GPIO.output(STEP, GPIO.LOW)
-        sleep(delay)
-        x += 1
-    curr_angle = angle
-    GPIO.cleanup()
+    def initialize(self,direction):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.DIR, GPIO.OUT)
+        GPIO.setup(self.STEP, GPIO.OUT)
+        GPIO.output(self.DIR,direction)
 
-rotate_motor(270)
-sleep(1)
-rotate_motor(200)
-sleep(1)
-rotate_motor(90)
+        MODE = (14, 15, 18)   # Microstep Resolution GPIO Pins
+        GPIO.setup(MODE, GPIO.OUT)
+        RESOLUTION = {'Full': (0, 0, 0),
+                    'Half': (1, 0, 0),
+                    '1/4': (0, 1, 0),
+                    '1/8': (1, 1, 0),
+                    '1/16': (0, 0, 1),
+                    '1/32': (1, 0, 1)}
+        GPIO.output(MODE, RESOLUTION['Full'])
+
+    def rotate_motor(self,angle,turbine = None):
+        angle_diff = angle - self.curr_angle
+        steps = int(abs(angle_diff) * 0.55556)
+        direction = 0 if angle_diff < 0 else 1
+        self.initialize(direction)
+        print(steps)
+        delay = .0208
+        for x in range(steps):
+            GPIO.output(self.STEP, GPIO.HIGH)
+            sleep(delay)
+            GPIO.output(self.STEP, GPIO.LOW)
+            sleep(delay)
+            x += 1
+            if turbine is not None:
+                turbine.rotate_motor(angle-1)
+        self.curr_angle = angle
+        GPIO.cleanup()

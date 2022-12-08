3512178aa8484b8f8fe4ad1b1f0dd7dee95606e1
nestor <nestordesabille4@gmail.com>
2022-12-09 00:40:58 +0800

    Start main.py

--- lib/raspi_stepper.py
+++ lib/raspi_stepper.py
@@ -31,12 +31,23 @@ class StepperMotor:
                     '1/32': (1, 0, 1)}
         GPIO.output(MODE, RESOLUTION['Full'])
 
-    def rotate_motor(self,angle,turbine = None):
-        angle_diff = angle - self.curr_angle
+    def rotate_motor(self,angle,turbine = None ):
+        curr_heading_angle = self.curr_angle + self.OFFSET
+        target_heading_angle = angle + self.OFFSET
+
+        if curr_heading_angle > 360:
+            curr_heading_angle -= 360
+
+        if target_heading_angle > 360:
+            target_heading_angle -= 360
+
+        angle_diff = target_heading_angle - curr_heading_angle
         steps = int(abs(angle_diff) * 0.55556)
+
+        angle_diff = target_heading_angle - self.curr_angle
         direction = 0 if angle_diff < 0 else 1
         self.initialize(direction)
-        print(steps)
+
         delay = .0208
         for x in range(steps):
             GPIO.output(self.STEP, GPIO.HIGH)
@@ -45,6 +56,9 @@ class StepperMotor:
             sleep(delay)
             x += 1
             if turbine is not None:
-                turbine.rotate_motor(angle-1)
+                turbine_angle = angle - (x+1)
+                if turbine_angle < 0:
+                    turbine_angle = 0 - turbine_angle
+                turbine.rotate_motor(angle-(x+1))
         self.curr_angle = angle
         GPIO.cleanup()
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from lib.raspi_stepper import StepperMotor
 from lib.sun_calc import SunCalc
-
+from lib.magnetometer import Magnetometer
 
 def main():
-    pass
+    magnetometer = Magnetometer()
+    offset = magnetometer.calculate_heading_angle()
+
+    solar_motor_x = StepperMotor(offset,27,22)
+    solar_motor_y = StepperMotor(offset,23,24)
+    wind_motor = StepperMotor(offset,20,21)
+
+    suntracker = SunCalc(10.35189,123.91335)
+
+    while True:
+        curr_sun_pos = suntracker.get_angle()
+        solar_motor_x.rotate_motor(curr_sun_pos[0],wind_motor)
 
 
 if __name__ == "__main__":
